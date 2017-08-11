e5cc5ee029e5aa7f98e549d5588b9a00dcced202
VR_room <VR_room>
2017-08-11 13:40:10 +0800

    Added initialization

--- initialization.py
+++ initialization.py
@@ -1,21 +1,22 @@
-import sys, os
+import sys, os, time
 import serial 
 
-
 ser = serial.Serial('COM4', baudrate=115200)  # Change COM number accordingly 
-print(ser.read(70))
 
 # b'c' means that 'c' string is encoded with bytes(ASCII)
-ser.write(b'y\r')
-ser.write(b'0')
-ser.write(b'1')
-ser.write(b'f')
-ser.write(b'd')
-ser.write(b'o')
 ser.write(b'c')
-ser.write(b't')
-ser.write(b'i')
-ser.write(b'm')
+
+# before you can control the iLimb there's a warm up of hand, written by I don;t know who
+print("everything is fine, just to checkout if the time gap is enough")
+time.sleep(5)
+
+# testing on the writing part
+ser.write(b'o')
+time.sleep(0.5)
 ser.write(b'r')
+
+# here test whether you can control the angle of motion by setting up the timing 
+time.sleep(0.4)
+ser.write(b's')
 
 ser.close()
